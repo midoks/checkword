394369e42223be56e512fe31c7d350225c8c6c1b
midoks <midoks163.com>
2018-04-26 18:54:54 +0800

    update

--- app/libs/words.go
+++ app/libs/words.go
@@ -2,10 +2,18 @@ package libs
 
 import (
 	// "bytes"
-	// "fmt"
+	"fmt"
+	"os"
 	// "math"
-	// "strings"
 	"github.com/astaxie/beego/logs"
+	"io/ioutil"
+	"strings"
+)
+
+const (
+	START  = 1
+	Middle = 2
+	END    = 3
 )
 
 type Node struct {
@@ -15,16 +23,54 @@ type Node struct {
 }
 
 type Words struct {
-	node []Node
+	root map[string]Node
 }
 
 func (this *Words) Init(path string) {
-	// os.
 
-	logs.Info(path)
+	dir_list, _ := ioutil.ReadDir(path)
+
+	for _, v := range dir_list {
+
+		if v.Name()[0:1] == "." {
+			continue
+		}
+
+		logs.Info(path+v.Name(), "reading")
+		f, err := os.Open(path + v.Name())
+		defer f.Close()
+
+		if err != nil {
+			logs.Warn(path, ":", err)
+		}
+		content, err := ioutil.ReadAll(f)
+		logs.Info(path+v.Name(), "end")
+
+		contentStr := strings.Trim(string(content), "*")
+
+		a := strings.Split(contentStr, "\r\n")
+
+		for i := 0; i < len(a); i++ {
+			this.Add(strings.Trim(a[i], "*"))
+		}
+	}
 }
 
-func (this *Words) Add() {
+func (this *Words) Add(word string) {
+	wRune := []rune(word)
+	fmt.Println(word)
+
+	var node *Node
+	for i := 0; i < len(wRune); i++ {
+
+		tmp := wRune[i : i+1]
+
+		node = new(Node)
+		node.name = string(tmp)
+		node.status = 0
+		fmt.Println(string(tmp))
+	}
+
 }
 
 func (this *Words) Del() {
@@ -32,5 +78,5 @@ func (this *Words) Del() {
 }
 
 func (this *Words) Update() {
-
+	fmt.Println(123)
 }
