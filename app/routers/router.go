cbddf3763d1b13196a825e0c84f48f4e72988b1a
midoks <midoks@163.com>
2018-07-04 14:53:43 +0800

    修改错误页面

--- app/routers/router.go
+++ app/routers/router.go
@@ -3,9 +3,15 @@ package routers
 import (
 	"github.com/astaxie/beego"
 	"github.com/midoks/checkword/app/controllers"
+	"net/http"
 )
 
+func page_not_found(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("page not found"))
+}
+
 func init() {
+	beego.ErrorHandler("404", page_not_found)
 
 	ns := beego.NewNamespace("/v1", beego.NSAutoRouter(&controllers.IndexController{}))
 	beego.AddNamespace(ns)
